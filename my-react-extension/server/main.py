49b831bd7cb09b809e13938f7d17d5ea0a99b8d7
Collin Qian <collinqian@umail.ucsb.edu>
2024-01-13 13:43:24 -0800

    trying to add cors

--- my-react-extension/server/main.py
+++ my-react-extension/server/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from server.config import Config
 from server.routes import main
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     app.register_blueprint(main)
 
