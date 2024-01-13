0e586f5452c60c32dd0cf53fa8626a3ab4206b04
jeffmun <61306390+Jumshim@users.noreply.github.com>
2024-01-12 21:22:54 -0800

    initialized react extension with flask

---
+++ popup.js
@@ -0,0 +1,6 @@
+const tabs = await chrome.tabs.query({
+  url: [
+    "https://developer.chrome.com/docs/webstore/*",
+    "https://developer.chrome.com/docs/extensions/*",
+  ],
+});
---
+++ webpack.config.production.js
@@ -0,0 +1,5 @@
+const defaultConfig = require("./webpack.config.js");
+module.exports = {
+    ...defaultConfig,
+    mode: "production",
+};
